ece8e93c7c3a37c3a4b7bcfbe7ffba2550483d77
AKXYN <akshayan027@gmail.com>
2024-04-10 12:30:30 +0530

    Restaurant name added in menu items

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { API_MENU_LINK, CLOUDINARY_IMG_PREFIX, API_MENU_LINK_PREFIX, API_MENU_LINK_SUFFIX } from "../utils/constants"
+import { CLOUDINARY_IMG_PREFIX, API_MENU_LINK_PREFIX, API_MENU_LINK_SUFFIX } from "../utils/constants"
 // import { json } from "react-router-dom"
 import { useParams } from "react-router-dom"
 
@@ -23,17 +23,24 @@ const RestaurantMenu = () => {
         fetchMenu()
     },[])
     const [menuFromAPI,setMenuFromAPI] = useState([])
+    const [categories,setcategories] = useState([])
+    const [resName,setResName] = useState("")
     const fetchMenu = async () => {
         const link = API_MENU_LINK_PREFIX+params.resId+API_MENU_LINK_SUFFIX
         const data = await fetch(link)
         const json = await data.json()
+        const categoriesFromAPI = json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards
         setMenuFromAPI(json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards)
-        console.log(json)
+        setResName(json?.data?.cards[0]?.card?.card?.text)
+        // console.log(json)
         console.log(menuFromAPI)
+        console.log(categoriesFromAPI)
+        const filteredCategories = categoriesFromAPI?.filter(category => (category?.card?.card?.["@type"]==="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")) // DO FROM HERE
+        console.log(filteredCategories)
     }
     return (
         <div>
-            <h1 className="font-bold m-8 text-3xl">Menu</h1>
+            <h1 className="font-bold m-8 text-3xl text-center">{resName}</h1>
             {menuFromAPI?.map(
             (menu) => <MenuCard 
             key={menu?.card?.info?.id}
