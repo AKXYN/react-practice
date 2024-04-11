98ac699a4fc5c7e0120a9f51d82c11a8904a1f17
AKXYN <akshayan027@gmail.com>
2024-04-11 16:32:00 +0530

    Shimmer UI fixed

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,5 +1,34 @@
+const ShimmerCard = () => {
+    return (
+    <div className="p-8 shadow-lg bg-gray-500 w-52 h-[400px] rounded-lg m-8">
+        <div className="bg-gray-300 h-8 w-36 rounded-lg"></div>
+        <div className="bg-gray-300 h-32 w-36 mt-12 rounded-lg"></div>
+        <div className="bg-gray-300 h-6 w-36 mt-4 rounded-lg"></div>
+        <div className="bg-gray-300 h-6 w-36 mt-4 rounded-lg"></div>
+        <div className="bg-gray-300 h-6 w-36 mt-4 rounded-lg"></div>
+    </div> 
+    )
+}
+
 const Shimmer = () => {
-    return <div className="flex flex-wrap h-full w-full">
+    return <div className="flex flex-wrap mt-24">
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        <ShimmerCard />
+        {/* <div className="h-64 w-40 bg-gray-400 border border-solid m-8"></div>
         <div className="h-64 w-40 bg-gray-400 border border-solid m-8"></div>
         <div className="h-64 w-40 bg-gray-400 border border-solid m-8"></div>
         <div className="h-64 w-40 bg-gray-400 border border-solid m-8"></div>
@@ -7,8 +36,7 @@ const Shimmer = () => {
         <div className="h-64 w-40 bg-gray-400 border border-solid m-8"></div>
         <div className="h-64 w-40 bg-gray-400 border border-solid m-8"></div>
         <div className="h-64 w-40 bg-gray-400 border border-solid m-8"></div>
-        <div className="h-64 w-40 bg-gray-400 border border-solid m-8"></div>
-        <div className="h-64 w-40 bg-gray-400 border border-solid m-8"></div>
+        <div className="h-64 w-40 bg-gray-400 border border-solid m-8"></div> */}
         {/* <div className="shimmer-card"></div>
         <div className="shimmer-card"></div>
         <div className="shimmer-card"></div>
