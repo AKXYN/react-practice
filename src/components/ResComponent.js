54c7e84a89745bccfecfe4409949fd4fa2486bf8
AKXYN <akshayan027@gmail.com>
2024-04-11 11:51:34 +0530

    Fixed restaurant image size consistency

--- src/components/ResComponent.js
+++ src/components/ResComponent.js
@@ -3,12 +3,12 @@ const ResComponent = (props) => {
     const {name,style,rating,time,link} = props
     return (
         <div className="p-8 border shadow-lg bg-purple-200 w-52 h-[400px] rounded-lg hover:bg-purple-300 hover:shadow-2xl hover:border-black">
-            <h3 className="font-bold ">{name}</h3>
+            <h3 className="font-bold h-20">{name}</h3>
             <img 
-            className="w-40 rounded-lg"
+            className="w-40 h-32 rounded-lg"
             src={link}
             />
-            <h4>{style}</h4>
+            <h4 className="mt-4">{style}</h4>
             <h4>{rating+" stars"}</h4>
             <h4>{time}</h4>
         </div>
